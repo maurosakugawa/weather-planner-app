948a9c93c9deb2a1dc2e663dfdffe88fbfa678c0
maurosakugawa <maurotoshiuki@gmail.com>
2026-05-18 08:59:50 -0300

    feat: setup tailwind and initial architecture

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-base-200">
+        <div className="card bg-base-100 shadow-xl p-10">
+            <h1 className="text-4xl font-bold">
+                Weather Planner App
+            </h1>
+            <p className="mt-4 text-base-content/70">
+                O dashboard aparecerá aqui.
+            </p>
+        </div>
+    </main>
+  );
+}
